f8448d619d74c6628046360fedd08ad81d5b67c3
Catalina Tamayo <Cata@MacBook-Pro-de-Catalina-3.local>
2019-09-09 11:39:01 -0300

    FILTRADO

--- src/data.js
+++ src/data.js
@@ -9,12 +9,24 @@
 
 // window.example = example;
 
-const data = () => {
+const dataPokemon = window.POKEMON.pokemon;
+window.dataPokemon=dataPokemon;
+ 
+const filterByType = (dataPokemon, valueFilter) => {
 
-  return "data";
-};
+    const resultType = dataPokemon.filter(Element => {
+        return Element.type.includes(valueFilter);
+  });
 
-function alphabet() {
-  var pokemonWeaknesses= [weaknesses]; 
+  return resultType;
+    }
 
-}
+    window.filterByType = filterByType;
+
+
+//Hacer funcion filtrar por tipo con argumentos genericos 
+//filterByType(dataPokemon,selectedType)
+//terminada la funcion declararla global:
+//window.filterByType=filterByType , de esta forma desde cualquier ventana
+//accedemos a la funcion filterByType de modo: window.filterByType
+
