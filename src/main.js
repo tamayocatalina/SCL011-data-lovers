82c7713a1f9a6df9821476bf22a3117ce9f45297
Your Name <you@example.com>
2019-09-10 10:37:59 -0300

    HU1 lista

--- src/main.js
+++ src/main.js
@@ -2,107 +2,107 @@
 const dataPokemon=window.POKEMON.pokemon;
 
 //declaro variables boton catálogo
-const opcionCatalogo= document.getElementById("btnCatalogo");
+const catalogueOption= document.getElementById("catalogueBtn");
 //declaro constante boton versus
-const opcionBusqueda= document.getElementById("btnBusqueda");
+const cataloqueSearch= document.getElementById("searchBtn");
 
-//Evento opcionCatalogo que es el btncatalogo
-opcionCatalogo.addEventListener("click", ()=>{
+//Evento catalogueOption que es el catalogueBtn
+catalogueOption.addEventListener("click", ()=>{
     //Declaro espacio que ocupará la impresión de las tarjetas
-    const catalogo=document.createElement("div");
-    catalogo.className="catalogo";
+    const catalogue=document.createElement("div");
+    catalogue.className="catalogue";
 
     //Muestro catálogo
-    document.getElementById("muestraCatalogo").style.display = 'block';
+    document.getElementById("showCatalogue").style.display = 'block';
     //Oculto versus
-    document.getElementById("muestraBusqueda").style.display = "none";
+    document.getElementById("showSearch").style.display = "none";
     //Recorro data para luego imprimir en contenedorpokemon
     for (let i=0; i<dataPokemon.length;i++){
 
         //Declaro espacio de las tarjetas
-        const espacioCatalogo=document.createElement("div");
+        const catalogueSpace=document.createElement("div");
         //declarar clase a la seccion para conectar con CSS
-        espacioCatalogo.className="espacioCatalogo";
+        catalogueSpace.className="catalogueSpace";
         //para declarar id se utiliza
-        //espacioCatalogo.id="vercatalogo";
+        //catalogueSpace.id="vercatalogo";
 
         //declaramos espacio donde se imprimirá la data de num
-        let espacioNum=document.createElement("h3");
+        let numSpace=document.createElement("h3");
         //le asignamos el contenido al espacio, especificamente el num del objeto
-        espacioNum.textContent=(dataPokemon[i].num);
+        numSpace.textContent=(dataPokemon[i].num);
         //declaramos clase de la sección num
-        espacioNum.className="espacioNum";
+        numSpace.className="numSpace";
 
         //declaramos espacio donde se imprimirá la data de name
-        let espacioName=document.createElement("h4");
-        espacioName.textContent=(dataPokemon[i].name);
-        espacioName.className="espacioName";
+        let nameSpace=document.createElement("h4");
+        nameSpace.textContent=(dataPokemon[i].name);
+        nameSpace.className="nameSpace";
 
         //declaramos espacio donde imprimirá la data img
-        let espacioImg=document.createElement("img");
-        espacioImg.src=(dataPokemon[i].img);
-        espacioImg.className="espacioImg";
+        let imgSpace=document.createElement("img");
+        imgSpace.src=(dataPokemon[i].img);
+        imgSpace.className="imgSpace";
 
         //Modal
-        const btnModal=document.createElement("button");
-        btnModal.textContent=("Ver más");
-        btnModal.className="estiloVerMas";
+        const modalBtn=document.createElement("button");
+        modalBtn.textContent=("Ver +");
+        modalBtn.className="seeMoreStyle";
 
-        btnModal.addEventListener("click", ()=>{
+        modalBtn.addEventListener("click", ()=>{
             //espacio contenedor del modal
-            const espacioModal=document.createElement("div");
-            espacioModal.className="espacioModal";
+            const modalSpace=document.createElement("div");
+            modalSpace.className="modalSpace";
 
-            let espacioNameModal=document.createElement("h4");
-            espacioNameModal.textContent=(dataPokemon[i].name);
-            espacioNameModal.className="estiloModal";
+            let modalNameSpace=document.createElement("h4");
+            modalNameSpace.textContent=(dataPokemon[i].name);
+            modalNameSpace.className="modalStyle";
 
-            let espacioImgModal=document.createElement("img");
-            espacioImgModal.src=(dataPokemon[i].img);
-            espacioImgModal.className="estiloModal";
+            let modalImgSpace=document.createElement("img");
+            modalImgSpace.src=(dataPokemon[i].img);
+            modalImgSpace.className="modalStyle";
 
-            let espacioHeightModal=document.createElement("p");
-            espacioHeightModal.textContent=(dataPokemon[i].height);
-            espacioHeightModal.className="estiloModal";
+            let modalHeightSpace=document.createElement("p");
+            modalHeightSpace.textContent=(dataPokemon[i].height);
+            modalHeightSpace.className="modalStyle";
 
-            let espacioWeightModal=document.createElement("p");
-            espacioWeightModal.textContent=(dataPokemon[i].weight);
-            espacioWeightModal.className="estiloModal";
+            let modalWeightSpace=document.createElement("p");
+            modalWeightSpace.textContent=(dataPokemon[i].weight);
+            modalWeightSpace.className="modalStyle";
 
-            const espacioButton=document.createElement("span");
-            espacioButton.className="estiloCerrar";
-            espacioButton.textContent=("Cerrar");
+            const buttonSpace=document.createElement("span");
+            buttonSpace.className="closeStyle";
+            buttonSpace.textContent=("X");
             
-            espacioModal.appendChild(espacioNameModal);
-            espacioModal.appendChild(espacioImgModal);
-            espacioModal.appendChild(espacioHeightModal);
-            espacioModal.appendChild(espacioWeightModal);
-            espacioCatalogo.appendChild(espacioModal);
+            modalSpace.appendChild(modalNameSpace);
+            modalSpace.appendChild(modalImgSpace);
+            modalSpace.appendChild(modalHeightSpace);
+            modalSpace.appendChild(modalWeightSpace);
+            catalogueSpace.appendChild(modalSpace);
 
-            espacioModal.appendChild(espacioButton);
+            modalSpace.appendChild(buttonSpace);
 
-            espacioButton.addEventListener("click", ()=>{
-                espacioModal.style.display="none";
+            buttonSpace.addEventListener("click", ()=>{
+                modalSpace.style.display="none";
                 });
 
         });
 
-        //asignamos al padre espacioCatalogo el hijo correspondiente
-        espacioCatalogo.appendChild(espacioImg);
-        espacioCatalogo.appendChild(espacioNum);
-        espacioCatalogo.appendChild(espacioName);
-        espacioCatalogo.appendChild(btnModal);
-        catalogo.appendChild(espacioCatalogo);
+        //asignamos al padre catalogueSpace el hijo correspondiente
+        catalogueSpace.appendChild(imgSpace);
+        catalogueSpace.appendChild(numSpace);
+        catalogueSpace.appendChild(nameSpace);
+        catalogueSpace.appendChild(modalBtn);
+        catalogue.appendChild(catalogueSpace);
 
         //llevamos al padre/div raizcatalogo de HTML el hijo/section de JS
-        document.getElementById("raizCatalogo").appendChild(catalogo).innerHTML;
+        document.getElementById("catalogueRoot").appendChild(catalogue).innerHTML;
     }
 });
 
-opcionBusqueda.addEventListener("click", ()=>{
-    document.getElementById('muestraBusqueda').style.display = 'block';
-    document.getElementById("muestraCatalogo").style.display = "none";
-    muestraBusqueda.innerHTML= "<p>Aquí irán los filtros</p>";
+cataloqueSearch.addEventListener("click", ()=>{
+    document.getElementById('showSearch').style.display = 'block';
+    document.getElementById("showCatalogue").style.display = "none";
+    showSearch.innerHTML= "<p>Aquí irán los filtros</p>";
 });
 
 
