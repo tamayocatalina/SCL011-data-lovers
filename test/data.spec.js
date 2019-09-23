9f721bf75e11511e9f40a6e2257e2ff21d2422f1
tamayocatalina <ftamayo.catalina@gmail.com>
2019-09-23 10:39:01 -0300

    calculo ok

--- test/data.spec.js
+++ test/data.spec.js
@@ -4,13 +4,37 @@ require('../src/data');
 require('./data.spec.js');
 
 
-describe('example', () => {
-  
+describe('filterByType()', () => {
+
+  const muestraPokemon= 
+  [{
+    "name": "Pikachu",
+    "type": [
+      "Electric"
+    ]
+  }, {
+    "name": "Sandshrew",
+    "type": [
+      "Ground"
+    ]
+  }, {
+    "name": "Sandslash",
+    "type": [
+      "Ground"
+    ]
+  }];
+
   it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+    assert.equal(typeof window.filterByType, 'function');
   });
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+  it('debería retornar "Pikachu" al filtrar por tipo Electrico', () => {
+    assert.deepEqual(window.filterByType(muestraPokemon, 'Electric'),   
+    [{
+      "name": "Pikachu",
+      "type": [
+        "Electric"
+      ]
+    }]);
   });
 })
